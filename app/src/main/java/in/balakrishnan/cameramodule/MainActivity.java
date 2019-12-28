944ebb2517b445c5acc294c0b6ab016a41df2216
balakrishnanfn <bala@nfnlabs.in>
2019-12-28 18:00:29 +0530

    Updated : Added method to get Result from EasyCam.

--- app/src/main/java/in/balakrishnan/cameramodule/MainActivity.java
+++ app/src/main/java/in/balakrishnan/cameramodule/MainActivity.java
@@ -3,13 +3,17 @@ package in.balakrishnan.cameramodule;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import in.balakrishnan.easycam.CameraBundleBuilder;
 import in.balakrishnan.easycam.CameraControllerActivity;
+import in.balakrishnan.easycam.FileUtils;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private String[] list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,5 +35,22 @@ public class MainActivity extends AppCompatActivity {
                 .setClearBucket(true)
                 .createCameraBundle());
         startActivityForResult(intent, 214);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 214) {
+            if (resultCode == RESULT_OK) {
+                assert data != null;
+                list = data.getStringArrayExtra("resultData");
+            }
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        FileUtils.clearAllFiles(this, getClass().getName());
     }
 }
